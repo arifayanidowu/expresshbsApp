5174d0e88e9502e51e09a6192489cb1640dd95c2
stizzle123 <arifayanidowu2@yahoo.com>
2018-06-29 14:46:08 +0100

    Edit /engineering

--- routes/engineering.js
+++ routes/engineering.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET engineering page. */
 router.get('/', function(req, res, next) {
-    res.render('engineering', { title: 'Engineering EPCI' });
+    res.render('engineering', { title: 'Engineering' });
 });
 
 module.exports = router;
